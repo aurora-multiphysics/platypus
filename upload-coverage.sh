7afdc02ccd15d72e1f6aef8996073b18b0b5baa7
Chris MacMackin <chris.macmackin@ukaea.uk>
2024-06-04 15:25:32 +0100

    Trying to use old approach to upload codecov

--- upload-coverage.sh
+++ upload-coverage.sh
@@ -2,13 +2,4 @@
 
 cd /opt/platypus
 
-curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
-curl -Os https://cli.codecov.io/latest/linux/codecov
-curl -Os https://cli.codecov.io/latest/linux/codecov.SHA256SUM
-curl -Os https://cli.codecov.io/latest/linux/codecov.SHA256SUM.sig
-gpg --verify codecov.SHA256SUM.sig codecov.SHA256SUM
-
-shasum -a 256 -c codecov.SHA256SUM
-chmod +x codecov
-
-./codecov --verbose
+bash <(curl -s https://codecov.io/bash)
