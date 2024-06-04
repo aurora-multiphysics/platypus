f13e99083fa414bc6da01e45298a2ab4564efed6
Chris MacMackin <chris.macmackin@ukaea.uk>
2024-06-04 11:52:16 +0100

    Trying to add code coverage reporting to CI

---
+++ upload-coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
+curl -Os https://cli.codecov.io/latest/linux/codecov
+curl -Os https://cli.codecov.io/latest/linux/codecov.SHA256SUM
+curl -Os https://cli.codecov.io/latest/linux/codecov.SHA256SUM.sig
+gpg --verify codecov.SHA256SUM.sig codecov.SHA256SUM
+
+shasum -a 256 -c codecov.SHA256SUM
+sudo chmod +x codecov
+./codecov --verbose upload-process --fail-on-error
