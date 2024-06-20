77d93f43697d6c67b5ad17c326a1503498a9245b
Edward Palmer <edward.palmer@ukaea.uk>
2024-06-20 15:18:47 +0000

    Fixes includes, removes spdlog dependency.

--- platypus.mk
+++ platypus.mk
@@ -4,16 +4,8 @@ include $(MFEM_DIR)/config/config.mk
 MFEM_INCLUDES 		:= -I$(MFEM_INC_DIR)/config -I$(MFEM_DIR)/ -I$(MFEM_DIR)/../miniapps/common
 MFEM_LIBS 			:= -L$(MFEM_DIR) -lmfem -lrt -L$(MFEM_DIR)/miniapps/common -lmfem-common $(MFEM_LIB)
 
-PLATYPUS_LIB		:= $(PLATYPUS_DIR)/lib
-
-SPDLOG_LIB 			 :=$(PLATYPUS_DIR)/build/_deps/spdlog-build/
-SPDLOG_INCLUDE 		 :=$(PLATYPUS_DIR)/build/_deps/spdlog-src/include/
-
-PLATYPUS_INCLUDE_LIB := $(sort $(dir $(shell find $(PLATYPUS_DIR)/src/ -name "*.hpp")))
-PLATYPUS_INCLUDE	 := $(foreach d, $(PLATYPUS_INCLUDE_LIB), -I$d) -I$(SPDLOG_INCLUDE)
-
 PLATYPUS_CXX_FLAGS 	:= -Wall $(MFEM_INCLUDES) -I$(MFEM_INC_DIR)/config
-PLATYPUS_LDFLAGS	:= -Wl,-rpath, $(MFEM_LIBS) -L$(SPDLOG_LIB) -lspdlog
+PLATYPUS_LDFLAGS	:= -Wl,-rpath, $(MFEM_LIBS)
 
 libmesh_CXXFlags 		+= $(PLATYPUS_CXX_FLAGS)
 libmesh_LDFLAGS 		+= $(PLATYPUS_LDFLAGS)
