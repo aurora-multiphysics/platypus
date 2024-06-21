c622019fb7535bb47eedb157bfc03909a9316e5c
Edward Palmer <edward.palmer@ukaea.uk>
2024-06-21 13:55:22 +0000

    Updates sapphire build script.

--- scripts/build-platypus-csd3-sapphire.sh
+++ scripts/build-platypus-csd3-sapphire.sh
@@ -267,14 +267,6 @@ function build_platypus() {
     git clone https://github.com/aurora-multiphysics/platypus.git
     cd platypus
     git checkout ${PLATYPUS_COMMIT}
-    git submodule update --init --recursive
-    cd contrib/hephaestus/
-    # git checkout master
-    mkdir build
-    cd build
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DMFEM_DIR=/$WORKDIR/mfem/build ..
-    ninja
-    cd /$WORKDIR/platypus
     make -j"$compile_cores"
 }
 
