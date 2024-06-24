666f9f87df8a94dcd17401fc19510c594ed9e2f0
Edward Palmer <edward.palmer@ukaea.uk>
2024-06-24 08:17:27 +0000

    All MFEM classes now inherit from MFEMGeneralUserObject.

--- include/auxkernels/MFEMAuxSolver.h
+++ include/auxkernels/MFEMAuxSolver.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "GeneralUserObject.h"
+#include "MFEMGeneralUserObject.h"
 #include "coefficients.h"
 #include "auxsolver_base.h"
 
-class MFEMAuxSolver : public GeneralUserObject
+class MFEMAuxSolver : public MFEMGeneralUserObject
 {
 public:
   static InputParameters validParams();
