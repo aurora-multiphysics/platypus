8423fd058a1952cf7289f67d9e40db09dc2c2772
Edward Palmer <edward.palmer@ukaea.uk>
2024-06-27 10:11:53 +0000

    Fixes and simplifies platypus.mk.

--- platypus.mk
+++ platypus.mk
@@ -1,17 +1,11 @@
-MFEM_DIR				:=$(APPLICATION_DIR)/../mfem/build
+MFEM_DIR			:=$(APPLICATION_DIR)/../mfem/build
 
 include $(MFEM_DIR)/config/config.mk
 MFEM_INCLUDES 		:= -I$(MFEM_INC_DIR)/config -I$(MFEM_DIR)/ -I$(MFEM_DIR)/../miniapps/common
 MFEM_LIBS 			:= -L$(MFEM_DIR) -lmfem -lrt -L$(MFEM_DIR)/miniapps/common -lmfem-common $(MFEM_LIB)
 
-PLATYPUS_CXX_FLAGS 	:= -Wall $(MFEM_INCLUDES) -I$(MFEM_INC_DIR)/config
-PLATYPUS_LDFLAGS	:= -Wl,-rpath, $(MFEM_LIBS)
-
-libmesh_CXXFlags 		+= $(PLATYPUS_CXX_FLAGS)
-libmesh_LDFLAGS 		+= $(PLATYPUS_LDFLAGS)
-
-ADDITIONAL_INCLUDES 	+= $(PLATYPUS_CXX_FLAGS)
-ADDITIONAL_LIBS 		+= $(PLATYPUS_LDFLAGS)
+ADDITIONAL_INCLUDES += $(MFEM_INCLUDES)
+ADDITIONAL_LIBS 	+= -Wl, $(MFEM_LIBS)
 
 $(info ADDITIONAL_INCLUDES = $(ADDITIONAL_INCLUDES));
 $(info ADDITIONAL_LIBS     = $(ADDITIONAL_LIBS));
