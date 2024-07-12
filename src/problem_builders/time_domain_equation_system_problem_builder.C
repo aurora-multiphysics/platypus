3c05b30a68163f7c0a9bd6b6a111135d1b658b5c
Edward Palmer <edward.palmer@ukaea.uk>
2024-07-12 10:51:53 +0000

    Moves ConstructOperator implementation into source file.

--- src/problem_builders/time_domain_equation_system_problem_builder.C
+++ src/problem_builders/time_domain_equation_system_problem_builder.C
@@ -12,4 +12,14 @@ TimeDomainEquationSystemProblemBuilder::InitializeKernels()
       _problem->_gridfunctions, _problem->_fespaces, _problem->_bc_map, _problem->_coefficients);
 }
 
+void
+TimeDomainEquationSystemProblemBuilder::ConstructOperator()
+{
+  auto equation_system = std::make_unique<platypus::TimeDependentEquationSystem>();
+  auto problem_operator = std::make_shared<platypus::TimeDomainEquationSystemProblemOperator>(
+      *_problem, std::move(equation_system));
+
+  _problem_operator = std::move(problem_operator);
+}
+
 } // namespace platypus
