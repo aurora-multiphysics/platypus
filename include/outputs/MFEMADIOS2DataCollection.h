acac226b7f0321754b138c142fbadd2d276a89d5
Alexander Blair <alexander.blair@ukaea.uk>
2024-10-04 15:53:31 +0100

    Add MFEMADIOS2DataCollection

---
+++ include/outputs/MFEMADIOS2DataCollection.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "MFEMDataCollection.h"
+#include "mfem.hpp"
+
+/**
+ * Class for output information saved in MFEM ADIOS2DataCollections
+ */
+class MFEMADIOS2DataCollection : public MFEMDataCollection
+{
+public:
+  static InputParameters validParams();
+
+  MFEMADIOS2DataCollection(const InputParameters & parameters);
+
+  std::shared_ptr<mfem::DataCollection>
+  createDataCollection(const std::string & collection_name) const override;
+
+protected:
+  unsigned int _refinements;
+};
