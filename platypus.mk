43af92cbf6b9d97e7d7e0612b3808534293a520e
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2024-11-26 13:57:22 -0800

    Do the bare minimum in platypus.mk

--- platypus.mk
+++ platypus.mk
@@ -1,11 +1,9 @@
-MFEM_DIR			?=$(APPLICATION_DIR)/../mfem/build
-MFEM_INC_DIR                    ?=$(MFEM_DIR)/include
+MFEM_DIR ?= $(APPLICATION_DIR)/../mfem/installed
 
-MFEM_INCLUDES 		:= -I$(MFEM_DIR)/include
-MFEM_LIBS 			:= -Wl,-rpath,$(MFEM_DIR)/lib -L$(MFEM_DIR)/lib -lmfem -lrt -lmfem-common
+include $(MFEM_DIR)/share/mfem/config.mk
 
-ADDITIONAL_INCLUDES += $(MFEM_INCLUDES)
-ADDITIONAL_LIBS 	+= $(MFEM_LIBS) ${MFEM_EXT_LIBS}
+ADDITIONAL_INCLUDES += $(MFEM_INCFLAGS)
+ADDITIONAL_LIBS     += $(MFEM_LIBS) -lmfem-common
 
 $(info ADDITIONAL_INCLUDES = $(ADDITIONAL_INCLUDES));
 $(info ADDITIONAL_LIBS     = $(ADDITIONAL_LIBS));
