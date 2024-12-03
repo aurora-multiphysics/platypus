4ffc74de061a254a20abb4a923e13a283079b28a
Alex Lindsay <alexander.lindsay@inl.gov>
2024-12-03 13:31:25 -0800

    Add support for properties restricted to block names

--- include/materials/MFEMMaterial.h
+++ include/materials/MFEMMaterial.h
@@ -9,7 +9,6 @@ class MFEMMaterial : public MFEMGeneralUserObject
 {
 public:
   static InputParameters validParams();
-  static std::vector<std::string> subdomainsToStrings(std::vector<SubdomainName> blocks);
   static libMesh::Point pointFromMFEMVector(const mfem::Vector & vec);
 
   MFEMMaterial(const InputParameters & parameters);
@@ -18,6 +17,8 @@ public:
   const std::vector<SubdomainName> & getBlocks() const { return _block_ids; }
 
 protected:
+  std::vector<std::string> subdomainsToStrings(std::vector<SubdomainName> blocks);
+
   std::vector<SubdomainName> _block_ids;
   platypus::PropertyManager & _properties;
 };
