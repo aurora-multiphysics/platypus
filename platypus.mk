e77e7b3b2be3214c28c1dc76a136917ad54943a9
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2024-12-19 10:16:47 +0000

    Remove unused parameters and variable warnings

--- platypus.mk
+++ platypus.mk
@@ -8,7 +8,7 @@ NEW_MFEM_INCFLAGS = $(shell echo "$(MFEM_INCFLAGS)" | sed -E 's/COMPILE_LANGUAGE
 MFEM_INCLUDES 		:= -I$(MFEM_INC_DIR)/config -I$(MFEM_DIR)/ -I$(MFEM_DIR)/../miniapps/common $(NEW_MFEM_INCFLAGS)
 MFEM_LIBS 			:= -L$(MFEM_DIR) -lmfem -lrt -L$(MFEM_DIR)/miniapps/common -lmfem-common $(MFEM_LIB)
 
-ADDITIONAL_INCLUDES += $(MFEM_INCLUDES)
+ADDITIONAL_INCLUDES += -Wno-unused-parameter -Wno-unused-variable $(MFEM_INCLUDES)
 ADDITIONAL_LIBS 	+= -Wl, $(MFEM_LIBS) ${MFEM_EXT_LIBS}
 
 $(info ADDITIONAL_INCLUDES = $(ADDITIONAL_INCLUDES));
