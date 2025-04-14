993b42dbf4248e3df53dca8de4f1d92fedc15138
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2025-04-14 16:19:40 +0100

    add SymFunctionPtr to coeff constructor

--- include/coeffs/MFEMScalarParsedCoeff.h
+++ include/coeffs/MFEMScalarParsedCoeff.h
@@ -9,20 +9,23 @@
 
 #include "mfem.hpp"
 #include "MFEMContainers.h"
+#include "FunctionParserUtils.h"
 #include <vector>
 #include <functional>
+
+
 
 class MFEMScalarParsedCoeff : public mfem::Coefficient
 {
   private:
     platypus::GridFunctions  & gFuncs;
     std::vector<std::string> & inputs;
-    std::function<double(std::vector<double>)> func;
+    FunctionParserUtils<false>::SymFunctionPtr & func;
 
   public:
     MFEMScalarParsedCoeff( platypus::GridFunctions  & gFuncs_
                  , std::vector<std::string> & inputs_
-                 , std::function<double(std::vector<double>)> func_);
+                 , FunctionParserUtils<false>::SymFunctionPtr & func_);
 
     double Eval(mfem::ElementTransformation &T, const mfem::IntegrationPoint &ip);
 };
--- src/coeffs/MFEMScalarParsedCoeff.C
+++ src/coeffs/MFEMScalarParsedCoeff.C
@@ -3,17 +3,23 @@
 
 MFEMScalarParsedCoeff::MFEMScalarParsedCoeff( platypus::GridFunctions  & gFuncs_
     , std::vector<std::string> & inputs_
-    , std::function<double(std::vector<double>)> func_)
+    , FunctionParserUtils<false>::SymFunctionPtr & func_)
     : gFuncs(gFuncs_), inputs(inputs_), func(func_){}
 
 double  MFEMScalarParsedCoeff::Eval(mfem::ElementTransformation &T, const mfem::IntegrationPoint &ip)
 {
-    std::vector<double> InpVals(inputs.size() + 4);   
-    for(unsigned I=0; I<inputs.size(); I++)
-    {
-        //if(){//Checks if its is a scalar GridFunction
-        //  InpVals[I] = gFuncs[inputs[I]].GetValue(T, ip);
-    }
+    std::vector<libMesh::Real> InpVals(inputs.size() + 4);
     
-    return func(InpVals);
+    mfem::Vector transip(3);
+    T.Transform(ip, transip);
+
+    for(unsigned i=0; i<inputs.size(); i++)
+      InpVals[i] = gFuncs.GetRef(inputs[i]).GetValue(T, ip);
+
+    for(unsigned i=0; i<3; i++)
+      InpVals[inputs.size()+i] = transip(i);
+    
+    InpVals[inputs.size()+3] = GetTime();
+    
+    return func->Eval(InpVals.data());
 }
