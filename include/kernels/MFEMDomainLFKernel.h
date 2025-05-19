2755809b56ffd2486bd39452f5da8e73f7fad895
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2025-05-19 16:05:14 +0100

    Add mbf divergence and domain lf kernels

---
+++ include/kernels/MFEMDomainLFKernel.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "MFEMKernel.h"
+
+/*
+(f, v)
+*/
+class MFEMDomainLFKernel : public MFEMKernel
+{
+public:
+  static InputParameters validParams();
+
+  MFEMDomainLFKernel(const InputParameters & parameters);
+  ~MFEMDomainLFKernel() override {}
+
+  virtual mfem::LinearFormIntegrator * createLFIntegrator() override;
+
+protected:
+  mfem::Coefficient & _coef;
+};
---
+++ src/kernels/MFEMVectorFEDivergenceKernel.C
@@ -0,0 +1,27 @@
+#include "MFEMVectorFEDivergenceKernel.h"
+
+registerMooseObject("PlatypusApp", MFEMVectorFEDivergenceKernel);
+
+InputParameters
+MFEMVectorFEDivergenceKernel::validParams()
+{
+  InputParameters params = MFEMMixedBilinearFormKernel::validParams();
+  params.addClassDescription(
+      "Adds the domain integrator to an MFEM problem for the mixed bilinear form "
+      "$(k \\vec \\nabla \\cdot \\vec u, v)_\\Omega$ arising from the weak form "
+      "of the divergence operator $k \\vec \\nabla \\cdot \\vec u$.");
+  params.addParam<MFEMScalarCoefficientName>("coefficient", "Name of property k to use.");
+  return params;
+}
+
+MFEMVectorFEDivergenceKernel::MFEMVectorFEDivergenceKernel(const InputParameters & parameters)
+  : MFEMMixedBilinearFormKernel(parameters),
+    _coef(getScalarCoefficient(getParam<MFEMScalarCoefficientName>("coefficient")))
+{
+}
+
+mfem::BilinearFormIntegrator *
+MFEMVectorFEDivergenceKernel::createMBFIntegrator()
+{
+  return new mfem::VectorFEDivergenceIntegrator(_coef);
+}
