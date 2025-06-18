145dcc543dbf80f8c2d1d95123911596adcd5fa3
SohailSTFC <133211007+SohailSTFC@users.noreply.github.com>
2025-06-18 17:40:12 +0100

    Update equation_system_problem_operator.C

--- src/problem_operators/equation_system_problem_operator.C
+++ src/problem_operators/equation_system_problem_operator.C
@@ -14,7 +14,7 @@ EquationSystemProblemOperator::Init(mfem::BlockVector & X)
 {
   ProblemOperator::Init(X);
 
-  GetEquationSystem()->BuildEquationSystem();
+  GetEquationSystem()->BuildEquationSystem(_problem._gridfunctions, _block_true_offsets);
 }
 
 void
