43a79404c395784cea0a145e5f6afce0f2fc4af0
SohailSTFC <133211007+SohailSTFC@users.noreply.github.com>
2025-06-19 00:13:29 +0100

    Update equation_system.C

--- src/equation_systems/equation_system.C
+++ src/equation_systems/equation_system.C
@@ -239,6 +239,51 @@ EquationSystem::BuildJacobian(mfem::BlockVector & trueX, mfem::BlockVector & tru
   FormLinearSystem(_jacobian, trueX, trueRHS);
 }
 
+void CopyVec(const mfem::Vector & x, mfem::Vector & y){ y = x;}
+
+void applyDirchValues(const mfem::Vector &k, mfem::Vector &y, mfem::Array<int> dofs)
+{
+  if(dofs.Size() > 0){ //Only apply if there are constrained DOF's
+
+    const bool use_dev = dofs.UseDevice() || k.UseDevice() || y.UseDevice();
+    const int n = dofs.Size();
+
+    // Use read+write access for X - we only modify some of its entries
+
+    auto d_X = y.ReadWrite(use_dev);
+    auto d_y = k.Read(use_dev);
+    auto d_dofs = dofs.Read(use_dev);
+
+    mfem::forall_switch(use_dev, n, [=] MFEM_HOST_DEVICE (int i)
+    {
+      const int dof_i = d_dofs[i];
+
+      if (dof_i >= 0)   d_X[dof_i]    =  d_y[dof_i];
+      if (!(dof_i >= 0))d_X[-1-dof_i] = -d_y[-1-dof_i];
+     });
+  }
+};
+
+void
+Update_timeVars(const mfem::real_t & dt, const mfem::real_t time, const mfem::Vector & X_Old)
+{
+  //Update the old vector
+  CopyVec(X_Old, _trueBlockX_Old);
+
+  //Update the xs boundary conditions
+
+
+  // Update the dxdts boundary conditions
+  for (int i = 0; i < _test_var_names.size(); i++)
+  {
+    auto & test_var_name = _trial_var_names.at(i);
+    CopyVec( *(_xs.at(i)), *(_dxdts.at(i)) );
+    *(_dxdts.at(i)) -= _trueBlockX_Old.GetBlock(i);
+    *(_dxdts.at(i)) /= dt;
+  }
+};
+
+
 void
 EquationSystem::Mult(const mfem::Vector & x, mfem::Vector & residual) const
 {
